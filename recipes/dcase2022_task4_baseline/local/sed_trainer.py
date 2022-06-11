9022d4bda5aba23b85ea16b0f23b7b4cf7cd68ec
Marco Bertola <mrc.bertola@gmail.com>
2022-06-11 15:00:56 +0200

    frame shift

--- recipes/dcase2022_task4_baseline/local/sed_trainer.py
+++ recipes/dcase2022_task4_baseline/local/sed_trainer.py
@@ -252,8 +252,8 @@ class SEDTask4(pl.LightningModule):
         do_add_reverb = False
         do_add_mixup = False
         do_add_noise = False
-        do_add_frame_shift = False
-        do_add_spec_augment = True
+        do_add_frame_shift = True
+        do_add_spec_augment = False
         """ Apply the training for one batch (a step). Used during trainer.fit
 
         Args:
